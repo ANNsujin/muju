afc5a525c634cf594357eb3e52162d50192c95fc
sujin Oh <sujinoh@sujinui-MacBook-Pro.local>
2016-08-03 18:19:04 +0900

    2016.08.03 room doing

--- muju/source/reservation.php
+++ muju/source/reservation.php
@@ -6,10 +6,291 @@
 		</div>
 		<hr class="menu_line">
 		<div class="contents">
-			<div class="desktop"><img src="#" arc="#"></div>
-			<div class="mob"><img src="#" arc="#"></div>
+			<!-- 방 설명 부분 -->
+			<div class="row">
+				<!-- 요금안내 part -->
+				<div class="col-lg-2 col-md-2 col-sm-2 col-xs-4 res_title">
+					<p> * 요금안내 </p>
+				</div>
+				<div class="col-lg-10 col-md-10 col-sm-10 col-xs-8">
+					<hr class="menu_line_sub">
+				</div>
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<!-- desktop -->
+					<table style="margin: 0 auto;" width="95%" class="room_table desktop">
+						<colgroup>
+							<col width="20%" />
+							<col width="10%" />
+							<col width="10%" />
+							<col width="10%" />
+							<col width="10%" />
+							<col width="10%" />
+							<col width="10%" />
+							<col width="10%" />
+							<col width="10%" />
+						</colgroup>
+						<tbody>
+							<tr>
+								<th class="r_title_down r_title_dark box_title" rowspan="2"><p> Price </p></th>
+								<th class="r_title_dark" colspan="2"> 인원 </th>
+								<th class="r_title_dark" colspan="6"> 요금 </th>
+							</tr>
+							<tr>
+								<td class="r_title_down r_title_light" style="font-weight:bold;"> 기준인원 </td>
+								<td class="r_title_down r_title_light" style="font-weight:bold;"> 추가인원 </td>
+								<td class="r_title_down r_title_light" colspan="2" style="font-weight:bold;"> 여름 성수기 </td>
+								<td class="r_title_down r_title_light" colspan="2" style="font-weight:bold;"> 겨울 성수기 </td>
+								<td class="r_title_down r_title_light" colspan="2" style="font-weight:bold;"> 비수기 </td>
+							</tr>
+							<tr>
+								<td> 1 Room, 1 Bed </td>
+								<td> 4 Persons </td>
+								<td> 4 Persons </td>
+								<td> 주중 </td>
+								<td> 주말 </td>
+								<td> 주중 </td>
+								<td> 주말 </td>
+								<td> 주중 </td>
+								<td> 주말 </td>
+							</tr>
+							<tr>
+								<td class="r_title_down" colspan="3"> 외부 데크 포함 22평 </td> <!-- 확인 필요 -->
+								<td class="r_title_down"> 200,000₩ </td>
+								<td class="r_title_down"> 200,000₩ </td>
+								<td class="r_title_down"> 170,000₩ </td>
+								<td class="r_title_down"> 200,000₩ </td>
+								<td class="r_title_down"> 150,000₩ </td>
+								<td class="r_title_down"> 180,000₩ </td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<!-- mobile -->
+				<div class="col-xs-12 mob">
+					<table style="margin: 0 auto; width:95%;" class="room_table">
+						<colgroup>
+							<col width="16%;" />
+							<col width="16%;" />
+							<col width="16%;" />
+							<col width="16%;" />
+							<col width="16%;" />
+							<col width="16%;" />
+						</colgroup>
+						<tbody>
+							<tr>
+								<th class="r_title_down r_title_dark box_title" colspan="4" rowspan="2"><p> Price </p></th>
+								<th class="r_title_dark" colspan="2"> 인원 </th>
+							</tr>
+							<tr>
+								<td class="r_title_down r_title_light" style="font-weight:bold;"> 기준인원 </td>
+								<td class="r_title_down r_title_light" style="font-weight:bold;"> 추가인원 </td>
+							</tr>
+							<tr>
+								<td colspan="4"> 1 Room, 1 Bed </td>
+								<td> 4인 </td>
+								<td> 4인 </td>
+							</tr>
+							<tr>
+								<td class="r_title_down" colspan="4"> 외부 데크 포함 22평 </td> <!-- 확인 필요 -->
+							</tr>
+							<tr>
+								<th class="r_title_dark" colspan="6"> 요금 </th>
+							</tr>
+							<tr>
+								<td class="r_title_down r_title_light" colspan="2" style="font-weight:bold;"> 여름 성수기 </td>
+								<td class="r_title_down r_title_light" colspan="2" style="font-weight:bold;"> 겨울 성수기 </td>
+								<td class="r_title_down r_title_light" colspan="2" style="font-weight:bold;"> 비수기 </td>
+							</tr>
+							<tr>
+								<td> 주중 </td>
+								<td> 주말 </td>
+								<td> 주중 </td>
+								<td> 주말 </td>
+								<td> 주중 </td>
+								<td> 주말 </td>
+							</tr>
+							<tr>
+								<td class="r_title_down"> 20만원 </td>
+								<td class="r_title_down"> 20만원 </td>
+								<td class="r_title_down"> 17만원 </td>
+								<td class="r_title_down"> 20만원 </td>
+								<td class="r_title_down"> 15만원 </td>
+								<td class="r_title_down"> 18만원 </td>
+							</tr>
+						</tbody>
+					</table><br>
+				</div>
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="col-lg-12 col-md-12 col-sm-12 res_bottom desktop">
+						<p> - 주중(일요일~목요일), 주말(금요일~토요일)<br>
+							- 여름 성수기 (7월 20일 ~ 8월 20일) | 겨울 성수기 (12월 25일 ~ 12월 30일, 1월 1일 ~ 2월 25일)<br>
+						    - 주말 요금 적용 : 12월 24일, 12월 31일, 공휴일 전날<br>
+						    - 기준 인원 초과시 : 1인당 10,000₩ 추가<br>
+						    - 바베큐 숯불 비용 : 방당 20,000₩ (숯불 이용 시, 현금 결제), 4인 초과시 30,000₩ </p>
+					<hr class="menu_line_sub" style="margin-left:0px; margin-top:20px;">
+					</div>
+					<div class="col-xs-12 res_bottom mob">
+						<p> - 주중(일요일~목요일), 주말(금요일~토요일)<br>
+							- 여름 성수기 (7월 20일 ~ 8월 20일)<br>
+							- 겨울 성수기 (12월 25일 ~ 12월 30일, 1월 1일 ~ 2월 25일)<br>
+						    - 주말 요금 적용 : 12월 24일, 12월 31일, 공휴일 전날<br>
+						    - 기준 인원 초과시 : 1인당 10,000₩ 추가<br>
+						    - 바베큐 숯불 비용 : 방당 20,000₩ (숯불 이용 시, 현금 결제)<br>
+						 	&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp4인 초과시 30,000₩ </p>
+					<hr class="menu_line_sub" style="margin-left:0px; margin-top:20px;">
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<!-- 유의사항 part -->
+				<div class="col-lg-2 col-md-2 col-sm-2 col-xs-4 res_title">
+					<p> * 유의사항 </p>
+				</div>
+				<div class="col-lg-10 col-md-10 col-sm-10 col-xs-8">
+					<hr class="menu_line_sub">
+				</div>
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<!-- desktop -->
+					<table style="margin: 0px auto 20px auto;" width="95%" class="res_table">
+						<tbody>
+							<tr>
+								<th class="b_title_down b_title_dark box_title"><p> 예약안내 </p></th>
+							</tr>
+							<tr>
+								<td class="desktop">
+									  - <b>유선상 예약과 온라인 예약으로 문의 받고 있습니다.</b><br>
+									  - 펜션 예약은 예약 후, 전액 입금하셔야 예약이 완료됩니다.<br>
+									  - 예약 후, <b>12시간 이내</b>로 이용 요금을 완불해주셔야 합니다. (미입금 시, 예약 취소로 간주합니다.)<br>
+									  - 예약금 입금은 예약자 이름으로 하셔야합니다. 다른 이름으로 입금 시, 꼭 유선상으로 알려주셔야 합니다.<br>
+									  - 기준 인원 초과 시에는 <b>1인당 10,000₩</b>의 추가 요금이 적용됩니다.<br>
+									  - 바베큐 그릴 이용 시에는 <b>방당 20,000₩</b>의 요금이 적용되며, 4인 초과시 10,000₩이 추가로 부과됩니다. <br>
+									  - 바베큐 그릴 이용료는 예약 시 지불하셔도 되며, 현장에서 현금 결제하셔도 됩니다.
+									  - 단, 예약 시에 바베큐 그릴 이용료를 지불하신 경우, 환불이 되지 않습니다.
+								</td>
+								<td class="mob">
+									  - <b>유선상 예약과 온라인 예약으로 문의 받고 있습니다.</b><br>
+									  - 펜션 예약은 예약 후, 전액 입금하셔야 예약이 완료됩니다.<br>
+									  - 예약 후, <b>12시간 이내</b>로 이용 요금을 완불해주셔야 합니다.<br>
+									  &nbsp&nbsp&nbsp(미입금 시, 예약 취소로 간주합니다.)<br>
+									  - 예약금 입금은 예약자 이름으로 하셔야합니다. <br>
+									  &nbsp&nbsp&nbsp다른 이름으로 입금 시, 꼭 유선상으로 알려주셔야 합니다.<br>
+									  - 기준 인원 초과 시, <b>1인당 1만원</b>의 추가 요금이 적용됩니다.<br>
+									  - 바베큐 그릴 이용 시, <b>방당 2만원</b>의 요금이 적용되며,<br>
+									  &nbsp&nbsp&nbsp4인 초과시 10,000₩이 추가로 부과됩니다.<br>
+									  - 바베큐 그릴 이용료는 예약 시 지불하셔도 되며,<br>
+									  &nbsp&nbsp&nbsp현장에서 현금 결제하셔도 됩니다.<br>
+									  - 단, 예약 시에 바베큐 그릴 이용료를 지불하신 경우,<br>
+									  &nbsp&nbsp&nbsp환불이 되지 않습니다.
+								</td>
+							</tr>
+							<tr>
+								<th class="b_title_down b_title_dark box_title"><p> 유의사항 안내 </p></th>
+							</tr>
+							<tr>
+								<td class="desktop">
+									  - 객실 및 기타 시설물은 다음 사람을 위하여 깨끗이 사용해 주세요.<br>
+									  - 기본적으로 픽업은 제공하지 않습니다.<br>
+									  - 애완견 입실은 금지입니다. <br>
+								      - 바베큐 이용 시, 외부 데크 바베큐장에서만 이용하실 수 있습니다. <br>
+								      -	지나친 고성방가나 폭죽은 자제해 주시기 바랍니다.<br>
+									  - 화재에 특별히 조심해주시기 바랍니다.<br>
+									  - 청소년은 혼숙이 불가능합니다.<br>
+									  - 청소년의 경우, 보호자의 허락이 있어야만 입실이 가능합니다.<br>
+									  - 음식물 및 기타 쓰레기는 깨끗한 환경을 위해 분리수거 해주세요.<br>
+									  - 기타 시설물 이용 시, 관리실 안내를 받으면 더욱 편리하게 사용하실 수 있습니다.
+								</td>
+								<td class="mob">
+									  - 객실 및 기타 시설물은 다음 사람을 위하여 깨끗이 사용해<br>
+									  &nbsp&nbsp&nbsp주세요.<br>
+									  - 기본적으로 픽업은 제공하지 않습니다.<br>
+									  - 애완견 입실은 금지입니다. <br>
+								      - 바베큐 이용 시, 외부 데크 바베큐장에서만 이용하실 수<br>
+									  &nbsp&nbsp&nbsp있습니다. <br>
+								      -	지나친 고성방가나 폭죽은 자제해 주시기 바랍니다.<br>
+									  - 화재에 특별히 조심해주시기 바랍니다.<br>
+									  - 청소년은 혼숙이 불가능합니다.<br>
+									  &nbsp&nbsp&nbsp이 경우, 보호자의 허락이 있어야만 입실이 가능합니다.<br>
+									  - 음식물 및 기타 쓰레기는 깨끗한 환경을 위해 분리수거<br>
+									  &nbsp&nbsp&nbsp해주세요.<br>
+									  - 기타 시설물 이용 시, 관리실 안내를 받으면 더욱 편리하게<br>
+									  &nbsp&nbsp&nbsp사용하실 수 있습니다.
+								</td>
+							</tr>
+							<tr>
+								<th class="b_title_down b_title_dark box_title"><p> 입/퇴실 안내사항 </p></th>
+							</tr>
+							<tr>
+								<td class="b_title_down desktop">
+									 					  - 입, 퇴실 시간은 <b>입실 2시, 퇴실 11시</b>를 원칙으로 합니다. 원활한 펜션 운영을 위해 꼭 지켜주세요.<br>
+									                      - 22시 이후에 도착할 시에는 사전에 연락을 해주셔야 합니다.<br>
+														  - 퇴실하기시 전에, 잊으신 물건이 없나 잘 살펴주시고, 시설물 및 소모품은 원위치에 정돈 부탁드립니다.<br>
+														  - 퇴실 시, <b>분리수거</b> 부탁드립니다.<br>
+														  - 퇴실 시, 파손된 물건이나 불편했던 점을 말씀해주시면 참고하겠습니다.
+								</td>
+								<td class="b_title_down mob">
+									 					  - 입, 퇴실 시간은<b> 입실 2시, 퇴실 11시</b>를 원칙으로 합니다.<br>
+														  &nbsp&nbsp&nbsp원활한 펜션 운영을 위해 꼭 지켜주세요.<br>
+									                      - 22시 이후에 도착할 시, 사전에 연락을 해주셔야 합니다.<br>
+														  - 퇴실하기시 전에, 잊으신 물건이 없나 잘 살펴주시고,<br>
+														  &nbsp&nbsp&nbsp시설물 및 소모품은 원위치에 정돈 부탁드립니다.<br>
+														  - 퇴실 시, <b>분리수거</b> 부탁드립니다.<br>
+														  - 퇴실 시, 파손된 물건이나 불편했던 점을 말씀해주시면<br>
+														  &nbsp&nbsp&nbsp참고하겠습니다.
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+			<div class="row">
+				<!-- 환불 규정안내 part -->
+				<div class="col-lg-2 col-md-2 col-sm-2 col-xs-4 res_title">
+					<p> * 환불규정 </p>
+				</div>
+				<div class="col-lg-10 col-md-10 col-sm-10 col-xs-8">
+					<hr class="menu_line_sub">
+				</div>
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<!-- desktop -->
+					<table style="margin: 0 auto;" width="95%" class="res_table">
+						<tbody>
+							<tr>
+								<th class="b_title_down b_title_dark box_title"><p> 환불 규정 안내 </p></th>
+							</tr>
+							<tr>
+								<td class="desktop">
+									 - 환불 적용은 이용요금 100% 입금 시에만 적용됩니다. <br>
+									 - 예약금 일부만 입금한 경우, 전액 환불 받지 못합니다.<br>
+									 - 예약 완료 후, 취소 시에는 환불 수수료가 있습니다.<br>
+									 - 예약 변경 시에 발생하는 수수료는 예약 취소 수수료와 동일합니다.<br>
+									 - 올바른 예약문화의 정착을 위하여 불가피한 조치이오니 양해 부탁드립니다.
+								</td>
+								<td class="mob">
+									- 환불 적용은 이용요금 100% 입금 시에만 적용됩니다. <br>
+									- 예약금 일부만 입금한 경우, 전액 환불 받지 못합니다.<br>
+									- 예약 완료 후, 취소 시에는 환불 수수료가 있습니다.<br>
+									- 예약 변경 시에 발생하는 수수료는 예약 취소 수수료와<br>
+									&nbsp&nbsp&nbsp동일합니다.<br>
+									- 올바른 예약문화의 정착을 위하여 불가피한 조치이오니<br>
+									&nbsp&nbsp&nbsp양해 부탁드립니다.
+								</td>
+							</tr>
+							<tr>
+								<th class="b_title_down b_title_dark box_title"><p> 예약 취소(변경) 수수료 안내 </p></th>
+							</tr>
+							<tr>
+								<td class="b_title_down"><b> * 기본 취소 수수료 10%</b><br>
+														  &nbsp&nbsp&nbsp이용일 7일 전 취소    : 입금액의 50% 환불<br>
+														  &nbsp&nbsp&nbsp이용일 5-6일 전 취소 : 입금액의 30% 환불<br>
+														  &nbsp&nbsp&nbsp이용일 0-4일 전 취소 : 환불 불가능<br><br>
+														  <b>- 예약 전에 날짜를 신중하게 생각하시고 결정해주세요.</b>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
 		</div>
 	</div>
-</div>
 </body>
 <? include("bottom.html") ?>
